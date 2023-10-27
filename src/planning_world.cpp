3241043170703c167c8245c4138d9f70bdf9437e
Xinsong Lin <zhongjiezhex12@me.com>
2023-10-27 13:39:55 -0700

    added a warning message for pointcloud density lower than 125000 dots/m^3

--- src/planning_world.cpp
+++ src/planning_world.cpp
@@ -36,8 +36,30 @@ void PlanningWorldTpl<DATATYPE>::setQpos(int const &index, VectorX const &state)
 template<typename DATATYPE>
 void PlanningWorldTpl<DATATYPE>::updatePointCloud(Matrixx3 const& vertices, double  const& resolution) {
     octomap::OcTree* tree = new octomap::OcTree(resolution);
-    for (size_t i = 0; i < vertices.rows(); i++) 
+    DATATYPE min_x = INT_MAX, min_y = INT_MAX, min_z = INT_MAX, max_x = INT_MIN, max_y = INT_MIN, max_z = INT_MIN;
+    for (size_t i = 0; i < vertices.rows(); i++) {
+        min_x = std::min(min_x, vertices(i, 0));
+        min_y = std::min(min_y, vertices(i, 1));
+        min_z = std::min(min_z, vertices(i, 2));
+        max_x = std::max(max_x, vertices(i, 0));
+        max_y = std::max(max_y, vertices(i, 1));
+        max_z = std::max(max_z, vertices(i, 2));
         tree->updateNode(octomap::point3d(vertices(i, 0), vertices(i, 1), vertices(i, 2)), true);
+    }
+    Vector3 O(min_x, min_y, min_z);
+    Vector3 A(min_x, max_y, min_z);
+    Vector3 B(max_x, min_y, min_z);
+    Vector3 C(min_x, min_y, max_z);
+    Vector3 OA = A - O;
+    Vector3 OB = B - O;
+    Vector3 OC = C - O;
+    // super crude approximation of the volume of the point cloud by taking the area of the parallelepiped
+    DATATYPE approximate_volume = std::abs(OA.cross(OB).dot(OC));
+    DATATYPE density = vertices.rows() / approximate_volume;
+    if (density < 125000) {  // this equates to roughly 2cm accuracy
+        std::cout << "Warning: approx. density of the point cloud is low: " << density << " points/m^3" << std::endl;
+        std::cout << "Collision might be inaccurate. If your shape is non-convex, please ignore." << std::endl;
+    }
 
     auto tree_ptr = std::shared_ptr<const octomap::OcTree>(tree);
     auto pose = Transform3::Identity();
