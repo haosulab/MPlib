a26c61b47ccc79b69b2bb4f7ff4d205b083dbf53
Kolin Guo <ruguo@ucsd.edu>
2024-01-19 15:38:10 -0800

    Add docstring to pybind_articulation.hpp

--- mplib/pymp/articulation.pyi
+++ mplib/pymp/articulation.pyi
@@ -14,7 +14,8 @@ M = typing.TypeVar("M", bound=int)
 
 class ArticulatedModel:
     """
-    Supports initialization from URDF and SRDF files, and provides access to underlying Pinocchio and FCL models.
+    Supports initialization from URDF and SRDF files, and provides access to
+    underlying Pinocchio and FCL models.
     """
     def __init__(
         self,
@@ -30,14 +31,15 @@ class ArticulatedModel:
     ) -> None:
         """
         Construct an articulated model from URDF and SRDF files.
-        Args:
-            urdf_filename: path to URDF file, can be relative to the current working directory
-            srdf_filename: path to SRDF file, we use it to disable self-collisions
-            gravity: gravity vector
-            joint_names: list of joints that are considered for planning
-            link_names: list of links that are considered for planning
-            verbose: print debug information
-            convex: use convex decomposition for collision objects
+
+        :param urdf_filename: path to URDF file, can be relative to the current working
+            directory
+        :param srdf_filename: path to SRDF file, we use it to disable self-collisions
+        :param gravity: gravity vector
+        :param joint_names: list of joints that are considered for planning
+        :param link_names: list of links that are considered for planning
+        :param verbose: print debug information
+        :param convex: use convex decomposition for collision objects
         """
     def get_base_pose(
         self,
@@ -46,64 +48,64 @@ class ArticulatedModel:
     ]:
         """
         Get the base pose of the robot.
-        Returns:
-            base pose of the robot in [x, y, z, qw, qx, qy, qz] format
+
+        :return: base pose of the robot in [x, y, z, qw, qx, qy, qz] format
         """
     def get_fcl_model(self) -> mplib.pymp.fcl.FCLModel:
         """
         Get the underlying FCL model.
-        Returns:
-            FCL model used for collision checking
+
+        :return: FCL model used for collision checking
         """
     def get_move_group_end_effectors(self) -> list[str]:
         """
         Get the end effectors of the move group.
-        Returns:
-            list of end effectors of the move group
+
+        :return: list of end effectors of the move group
         """
     def get_move_group_joint_indices(self) -> list[int]:
         """
         Get the joint indices of the move group.
-        Returns:
-            list of user joint indices of the move group
+
+        :return: list of user joint indices of the move group
         """
     def get_move_group_joint_names(self) -> list[str]:
         """
         Get the joint names of the move group.
-        Returns:
-            list of joint names of the move group
+
+        :return: list of joint names of the move group
         """
     def get_move_group_qpos_dim(self) -> int:
         """
         Get the dimension of the move group qpos.
-        Returns:
-            dimension of the move group qpos
+
+        :return: dimension of the move group qpos
         """
     def get_pinocchio_model(self) -> mplib.pymp.pinocchio.PinocchioModel:
         """
         Get the underlying Pinocchio model.
-        Returns:
-            Pinocchio model used for kinematics and dynamics computations
+
+        :return: Pinocchio model used for kinematics and dynamics computations
         """
     def get_qpos(
         self,
     ) -> numpy.ndarray[tuple[M, typing.Literal[1]], numpy.dtype[numpy.float64]]:
         """
         Get the current joint position of all active joints inside the URDF.
-        Returns:
-            current qpos of all active joints
+
+        :return: current qpos of all active joints
         """
     def get_user_joint_names(self) -> list[str]:
         """
         Get the joint names that the user has provided for planning.
-        Returns:
-            list of joint names of the user
+
+        :return: list of joint names of the user
         """
     def get_user_link_names(self) -> list[str]:
         """
         Get the link names that the user has provided for planning.
-        Returns:
-            list of link names of the user
+
+        :return: list of link names of the user
         """
     def set_base_pose(
         self,
@@ -113,22 +115,24 @@ class ArticulatedModel:
     ) -> None:
         """
         Set the base pose of the robot.
-        Args:
-            pose: base pose of the robot in [x, y, z, qw, qx, qy, qz] format
+
+        :param pose: base pose of the robot in [x, y, z, qw, qx, qy, qz] format
         """
     @typing.overload
     def set_move_group(self, end_effector: str) -> None:
         """
-        Set the move group, i.e. the chain ending in end effector for which to compute the forward kinematics for all subsequent queries.
-        Args:
-            chain: list of links extending to the end effector
+        Set the move group, i.e. the chain ending in end effector for which to compute
+        the forward kinematics for all subsequent queries.
+
+        :param chain: list of links extending to the end effector
         """
     @typing.overload
     def set_move_group(self, end_effectors: list[str]) -> None:
         """
-        Set the move group but we have multiple end effectors in a chain. i.e. Base --> EE1 --> EE2 --> ... --> EEn
-        Args:
-            end_effectors: names of the end effector link
+        Set the move group but we have multiple end effectors in a chain. I.e., Base -->
+        EE1 --> EE2 --> ... --> EEn
+
+        :param end_effectors: names of the end effector link
         """
     def set_qpos(
         self,
@@ -137,13 +141,15 @@ class ArticulatedModel:
     ) -> None:
         """
         Let the planner know the current joint positions.
-        Args:
-            qpos: current qpos of all active joints or just the move group joints
-            full: whether to set the full qpos or just the move group qpos. if full false, we will pad the missing joints with current known qpos. the default is false
+
+        :param qpos: current qpos of all active joints or just the move group joints
+        :param full: whether to set the full qpos or just the move group qpos. If full
+            is ``False``, we will pad the missing joints with current known qpos. The
+            default is ``False``
         """
     def update_SRDF(self, SRDF: str) -> None:
         """
         Update the SRDF file to disable self-collisions.
-        Args:
-            srdf: path to SRDF file, can be relative to the current working directory
+
+        :param srdf: path to SRDF file, can be relative to the current working directory
         """
--- src/articulated_model.h
+++ src/articulated_model.h
@@ -5,6 +5,10 @@
 #include "macros_utils.hpp"
 #include "pinocchio_model.h"
 
+/**
+ * Supports initialization from URDF and SRDF files, and provides access to
+ * underlying Pinocchio and FCL models.
+ */
 template <typename DATATYPE>
 class ArticulatedModelTpl {
  private:
@@ -31,51 +35,143 @@ class ArticulatedModelTpl {
   Transform3 base_tf;
 
  public:
+  /**
+   * Construct an articulated model from URDF and SRDF files.
+   *
+   * @param urdf_filename: path to URDF file, can be relative to the current working
+   *  directory
+   * @param srdf_filename: path to SRDF file, we use it to disable self-collisions
+   * @param gravity: gravity vector
+   * @param joint_names: list of joints that are considered for planning
+   * @param link_names: list of links that are considered for planning
+   * @param verbose: print debug information
+   * @param convex: use convex decomposition for collision objects
+   */
   ArticulatedModelTpl(const std::string &urdf_filename,
                       const std::string &srdf_filename, const Vector3 &gravity,
                       const std::vector<std::string> &joint_names = {},
                       const std::vector<std::string> &link_names = {},
                       const bool &verbose = true, const bool &convex = false);
 
+  /**
+   * Get the underlying Pinocchio model.
+   *
+   * @return: Pinocchio model used for kinematics and dynamics computations
+   */
   PinocchioModelTpl<DATATYPE> &getPinocchioModel() { return pinocchio_model; }
 
+  /**
+   * Get the underlying FCL model.
+   *
+   * @return: FCL model used for collision checking
+   */
   FCLModelTpl<DATATYPE> &getFCLModel() { return fcl_model; }
 
+  /**
+   * Set the move group, i.e. the chain ending in end effector for which to compute the
+   *  forward kinematics for all subsequent queries.
+   *
+   * @param chain: list of links extending to the end effector
+   */
   void setMoveGroup(const std::string &end_effector);
 
+  /**
+   * Set the move group but we have multiple end effectors in a chain.
+   * I.e., Base --> EE1 --> EE2 --> ... --> EEn
+   *
+   * @param end_effectors: names of the end effector link
+   */
   void setMoveGroup(const std::vector<std::string> &end_effectors);
 
+  /**
+   * Get the joint indices of the move group.
+   *
+   * @return: list of user joint indices of the move group
+   */
   std::vector<size_t> getMoveGroupJointIndices(void) { return move_group_user_joints; }
 
+  /**
+   * Get the joint names of the move group.
+   *
+   * @return: list of joint names of the move group
+   */
   std::vector<std::string> getMoveGroupJointName(void);
 
+  /**
+   * Get the joint names that the user has provided for planning.
+   *
+   * @return: list of joint names of the user
+   */
   std::vector<std::string> getUserJointNames(void) { return user_joint_names; }
 
+  /**
+   * Get the link names that the user has provided for planning.
+   *
+   * @return: list of link names of the user
+   */
   std::vector<std::string> getUserLinkNames(void) { return user_link_names; }
 
+  /**
+   * Get the end effectors of the move group.
+   *
+   * @return: list of end effectors of the move group
+   */
   std::vector<std::string> getMoveGroupEndEffectors(void) {
     return move_group_end_effectors;
   }
 
+  /**
+   * Get the dimension of the move group qpos.
+   *
+   * @return: dimension of the move group qpos
+   */
   size_t getQposDim(void) { return move_group_qpos_dim; }
 
+  /**
+   * Get the current joint position of all active joints inside the URDF.
+   *
+   * @return: current qpos of all active joints
+   */
   VectorX getQpos(void) { return current_qpos; }
 
+  /**
+   * Let the planner know the current joint positions.
+   *
+   * @param qpos: current qpos of all active joints or just the move group joints
+   * @param full: whether to set the full qpos or just the move group qpos.
+   *    If full is ``false``, we will pad the missing joints with current known qpos.
+   *    The default is ``false``
+   */
   void setQpos(const VectorX &qpos, const bool &full = false);
 
-  /** only support one end effector case */
+  /** Only support one end effector case */
   size_t getEEFrameIndex() {
     return std::find(user_link_names.begin(), user_link_names.end(),
                      move_group_end_effectors[0]) -
            user_link_names.begin();
   }
 
+  /**
+   * Update the SRDF file to disable self-collisions.
+   *
+   * @param srdf: path to SRDF file, can be relative to the current working directory
+   */
   void updateSRDF(const std::string &srdf) {
     fcl_model.removeCollisionPairsFromSrdf(srdf);
   }
 
+  /**
+   * Set the base pose of the robot.
+   *
+   * @param pose: base pose of the robot in [x, y, z, qw, qx, qy, qz] format
+   */
   void setBasePose(const Vector7 &pose);
 
+  /**
+   * Get the base pose of the robot.
+   *
+   * @return: base pose of the robot in [x, y, z, qw, qx, qy, qz] format
+   */
   Vector7 getBasePose() { return base_pose; }
 };
 
