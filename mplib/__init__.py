58a02164417f2db23e85bae1049bebb043f0b784
Kolin Guo <ruguo@ucsd.edu>
2024-01-21 20:37:08 -0800

    Unclear wildcard imports should be avoided

--- mplib/__init__.py
+++ mplib/__init__.py
@@ -5,7 +5,7 @@
 from importlib.metadata import version
 
 from mplib.planner import Planner
-from mplib.pymp import *
+from mplib.pymp import articulation, fcl, kdl, ompl, pinocchio, planning_world
 
 # from .pymp import set_global_seed
 
