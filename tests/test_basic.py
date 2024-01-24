cbe11eb8125705dfe68e010abb60ed875eabae2b
Xinsong Lin <zhongjiezhex12@me.com>
2024-01-23 22:24:39 -0800

    getting started with tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,54 +1,38 @@
 import numpy as np
-
+import unittest
 from mplib import Planner
 
 PANDA_SPEC = {
-    "urdf": "data/panda/panda.urdf",
-    "srdf": "data/panda/panda.srdf",
-    "user_link_names": [
-        "panda_link0",
-        "panda_link1",
-        "panda_link2",
-        "panda_link3",
-        "panda_link4",
-        "panda_link5",
-        "panda_link6",
-        "panda_link7",
-        "panda_link8",
-        "panda_hand",
-        "panda_leftfinger",
-        "panda_rightfinger",
-    ],
-    "user_joint_names": [
-        "panda_joint1",
-        "panda_joint2",
-        "panda_joint3",
-        "panda_joint4",
-        "panda_joint5",
-        "panda_joint6",
-        "panda_joint7",
-        "panda_finger_joint1",
-        "panda_finger_joint2",
-    ],
-    "move_group": "panda_hand",
-    "joint_vel_limits": np.ones(7),
-    "joint_acc_limits": np.ones(7),
+  "urdf": "data/panda/panda.urdf",
+  "srdf": "data/panda/panda.srdf",
+  "move_group": "panda_hand",
 }
 
+class TestPlannerSimple(unittest.TestCase):
+  def __init__(self, methodName: str = "runTest") -> None:
+    super().__init__(methodName)
+    self.planner = Planner(**PANDA_SPEC)
+    self.target_pose = [0.4, 0.3, 0.12, 0, 1, 0, 0]
+    self.init_qpos = np.array([0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0, 0])
 
-def test_plan():
-    planner = Planner(**PANDA_SPEC)
-    pose = [0.4, 0.3, 0.12, 0, 1, 0, 0]
-    qpos = np.array([0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0, 0])
-    planner.plan(pose, qpos)
+  def get_end_effector_pose(self):
+    ee_idx = self.planner.link_name_2_idx[self.planner.move_group]
+    return self.planner.robot.get_pinocchio_model().get_link_pose(ee_idx)
 
+  def test_planner(self):
+    result_sampling = self.planner.plan_qpos_to_pose(self.target_pose, self.init_qpos)
+    self.assertEqual(result_sampling["status"], "Success")
+    last_qpos = result_sampling["position"][-1]
+    self.planner.robot.set_qpos(last_qpos)
+    self.assertTrue(np.allclose(self.get_end_effector_pose(), self.target_pose, atol=1e-2))
 
-def test_plan_screw():
-    planner = Planner(**PANDA_SPEC)
-    pose = [0.4, 0.3, 0.12, 0, 1, 0, 0]
-    qpos = np.array([0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0, 0])
-    planner.plan_screw(pose, qpos)
+  def test_planner_screw(self):
+    result_sampling = self.planner.plan_screw(self.target_pose, self.init_qpos)
+    self.assertEqual(result_sampling["status"], "Success")
+    last_qpos = result_sampling["position"][-1]
+    self.planner.robot.set_qpos(last_qpos)
+    self.assertTrue(np.allclose(self.get_end_effector_pose(), self.target_pose, atol=1e-2))
 
 
 if __name__ == "__main__":
-    test_plan()
+  unittest.main()
