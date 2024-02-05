7119c94af04c0d432df8bc8889ae7f15481f8dc7
Kolin Guo <ruguo@ucsd.edu>
2024-02-05 14:51:06 -0800

    Switch to use sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,5 +42,5 @@ def setup(app):
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
