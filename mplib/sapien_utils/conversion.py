4b0c6b5a3565bd79afd828fd4f2bb61abe6dbe4f
Kolin Guo <ruguo@ucsd.edu>
2024-03-31 22:42:04 -0700

    Add mplib.sapien_utils: create PlanningWorld from sapien Scene
    Copied 16450be,4c3b7aa,c4fb706,2c83a43 from kolinguo/MPlib

---
+++ mplib/sapien_utils/conversion.py
@@ -0,0 +1,205 @@
+from __future__ import annotations
+
+import numpy as np
+from sapien import Entity, Pose, Scene
+from sapien.physx import (
+    PhysxArticulation,
+    PhysxArticulationLinkComponent,
+    PhysxCollisionShapeBox,
+    PhysxCollisionShapeCapsule,
+    PhysxCollisionShapeConvexMesh,
+    PhysxCollisionShapeCylinder,
+    PhysxCollisionShapePlane,
+    PhysxCollisionShapeSphere,
+    PhysxCollisionShapeTriangleMesh,
+    PhysxRigidBaseComponent,
+)
+from transforms3d.euler import euler2quat
+
+from ..pymp import ArticulatedModel, PlanningWorld
+from ..pymp.collision_detection.fcl import (
+    Box,
+    Capsule,
+    CollisionObject,
+    Convex,
+    Cylinder,
+    Halfspace,
+    Sphere,
+    collide,
+    distance,
+)
+from .srdf_exporter import export_srdf
+from .urdf_exporter import export_kinematic_chain_urdf
+
+
+class SapienPlanningWorld(PlanningWorld):
+    def __init__(self, sim_scene: Scene, planned_articulation_names: list[str] = []):
+        """
+        Creates an mplib.pymp.planning_world.PlanningWorld from a sapien.Scene.
+
+        :param planned_articulation_names: name of planned articulations.
+        """
+        super().__init__([], [])
+        self._sim_scene = sim_scene
+
+        articulations: list[PhysxArticulation] = sim_scene.get_all_articulations()
+        actors: list[Entity] = sim_scene.get_all_actors()
+
+        for articulation in articulations:
+            urdf_str = export_kinematic_chain_urdf(articulation)
+            srdf_str = export_srdf(articulation)
+
+            # Get all links with collision shapes at local_pose
+            collision_links = []  # [(link_name, [CollisionObject, ...]), ...]
+            for link in articulation.links:
+                col_objs = self.convert_sapien_col_shape(link)
+                if len(col_objs) > 0:
+                    collision_links.append((link.name, col_objs))
+
+            articulated_model = ArticulatedModel.create_from_urdf_string(
+                urdf_str,
+                srdf_str,
+                collision_links=collision_links,
+                gravity=[0, 0, -9.81],
+                joint_names=[j.name for j in articulation.active_joints],
+                link_names=[l.name for l in articulation.links],
+                verbose=False,
+            )
+            articulated_model.set_qpos(articulation.qpos)  # set_qpos to update poses
+
+            self.add_articulation(articulation.name, articulated_model)
+
+        for articulation_name in planned_articulation_names:
+            self.set_articulation_planned(articulation_name, True)
+
+        for entity in actors:
+            component = entity.find_component_by_type(PhysxRigidBaseComponent)
+            assert (
+                component is not None
+            ), f"No PhysxRigidBaseComponent found in {entity.name}: {entity.components=}"
+            assert not isinstance(
+                component, PhysxArticulationLinkComponent
+            ), f"Component should not be PhysxArticulationLinkComponent: {component=}"
+
+            # Convert collision shapes at current global pose
+            col_objs = self.convert_sapien_col_shape(component)
+            # TODO: multiple collision shapes
+            assert len(col_objs) == 1, (
+                f"Should only have 1 collision object, got {len(col_objs)} shapes for "
+                f"entity '{entity.name}'"
+            )
+
+            self.add_normal_object(entity.name, col_objs[0])
+
+    def update_from_simulation(self, update_attached_object: bool = True) -> None:
+        """Updates planning_world articulations/objects pose with current Scene state
+
+        :param update_attached_object: whether to update the attached pose of
+                                    all attached objects
+        """
+        for articulation in self._sim_scene.get_all_articulations():
+            # set_qpos to update poses
+            self.get_articulation(articulation.name).set_qpos(articulation.qpos)
+
+        for entity in self._sim_scene.get_all_actors():
+            component = entity.find_component_by_type(PhysxRigidBaseComponent)
+            assert (
+                component is not None
+            ), f"No PhysxRigidBaseComponent found in {entity.name}: {entity.components=}"
+            assert not isinstance(
+                component, PhysxArticulationLinkComponent
+            ), f"Component should not be PhysxArticulationLinkComponent: {component=}"
+
+            shapes = component.collision_shapes
+            # TODO: multiple collision shapes
+            assert len(shapes) == 1, (
+                f"Should only have 1 collision shape, got {len(shapes)} shapes for "
+                f"entity '{entity.name}'"
+            )
+            shape = shapes[0]
+
+            pose: Pose = entity.pose * shape.local_pose
+            # NOTE: Convert poses for Capsule/Cylinder
+            if isinstance(
+                shape, (PhysxCollisionShapeCapsule, PhysxCollisionShapeCylinder)
+            ):
+                pose = pose * Pose(q=euler2quat(0, np.pi / 2, 0))
+
+            # handle attached object
+            if self.is_normal_object_attached(entity.name):
+                attached_body = self.get_attached_object(entity.name)
+                if update_attached_object:
+                    parent_posevec = (
+                        attached_body.get_attached_articulation()
+                        .get_pinocchio_model()
+                        .get_link_pose(attached_body.get_attached_link_id())
+                    )
+                    parent_pose = Pose(parent_posevec[:3], parent_posevec[3:])
+                    pose = parent_pose.inv() * pose  # new attached pose
+                    attached_body.set_pose(np.hstack((pose.p, pose.q)))
+                attached_body.update_pose()
+            else:
+                self.get_normal_object(entity.name).set_transformation(
+                    np.hstack((pose.p, pose.q))
+                )
+
+    @staticmethod
+    def convert_sapien_col_shape(
+        component: PhysxRigidBaseComponent,
+    ) -> list[CollisionObject]:
+        """Converts a SAPIEN physx.PhysxRigidBaseComponent to an FCL CollisionObject
+        Returns a list of collision_obj at their current poses.
+
+        If the component is an articulation link, the returned collision_obj is at
+        the shape's local_pose.
+        Otherwise, the returned collision_obj is at the entity's global pose
+        """
+        shapes = component.collision_shapes
+        if len(shapes) == 0:
+            return []
+
+        # NOTE: MPlib currently only supports 1 collision shape per object
+        # TODO: multiple collision shapes
+        assert len(shapes) == 1, (
+            f"Should only have 1 collision shape, got {len(shapes)} shapes for "
+            f"entity '{component.entity.name}'"
+        )
+
+        shape = shapes[0]
+        if isinstance(component, PhysxArticulationLinkComponent):  # articulation link
+            pose = shape.local_pose
+        else:
+            pose = component.entity.pose * shape.local_pose
+
+        if isinstance(shape, PhysxCollisionShapeBox):
+            collision_geom = Box(side=shape.half_size * 2)
+        elif isinstance(shape, PhysxCollisionShapeCapsule):
+            collision_geom = Capsule(radius=shape.radius, lz=shape.half_length * 2)
+            # NOTE: physx Capsule has x-axis along capsule height
+            # FCL Capsule has z-axis along capsule height
+            pose = pose * Pose(q=euler2quat(0, np.pi / 2, 0))
+        elif isinstance(shape, PhysxCollisionShapeConvexMesh):
+            assert np.allclose(
+                shape.scale, 1.0
+            ), f"Not unit scale {shape.scale}, need to rescale vertices?"
+            collision_geom = Convex(vertices=shape.vertices, faces=shape.triangles)
+        elif isinstance(shape, PhysxCollisionShapeCylinder):
+            collision_geom = Cylinder(radius=shape.radius, lz=shape.half_length * 2)
+            # NOTE: physx Cylinder has x-axis along cylinder height
+            # FCL Cylinder has z-axis along cylinder height
+            pose = pose * Pose(q=euler2quat(0, np.pi / 2, 0))
+        elif isinstance(shape, PhysxCollisionShapePlane):
+            raise NotImplementedError(
+                "Support for Plane collision is not implemented yet in mplib, "
+                "need fcl::Plane"
+            )
+        elif isinstance(shape, PhysxCollisionShapeSphere):
+            collision_geom = Sphere(radius=shape.radius)
+        elif isinstance(shape, PhysxCollisionShapeTriangleMesh):
+            # NOTE: see mplib.pymp.fcl.Triangle
+            raise NotImplementedError(
+                "Support for TriangleMesh collision is not implemented yet."
+            )
+        else:
+            raise TypeError(f"Unknown shape type: {type(shape)}")
+        return [CollisionObject(collision_geom, pose.p, pose.q)]
---
+++ mplib/sapien_utils/urdf_exporter.py
@@ -0,0 +1,192 @@
+from __future__ import annotations
+
+from xml.etree import ElementTree as ET
+
+from sapien.physx import (
+    PhysxArticulation,
+    PhysxArticulationJoint,
+    PhysxArticulationLinkComponent,
+)
+
+
+def export_link(link: PhysxArticulationLinkComponent) -> ET.Element:
+    """
+    Export an articulation link. Generates
+    <link name="world">
+        <inertial>
+        <origin rpy="0.0 -0.0 0.0" xyz="0.0 0.0 0.0"></origin>
+        <mass value="9.999999974752427e-07"></mass>
+        <inertia ixx="1e-06" ixy="0" ixz="0" iyy="1e-06" iyz="0" izz="1e-06"></inertia>
+        </inertial>
+    </link>
+    """
+    cmass_pose = link.cmass_local_pose
+    angles = cmass_pose.rpy
+
+    elem_link = ET.Element("link", {"name": link.name})
+    elem_inertial = ET.SubElement(elem_link, "inertial")
+    ET.SubElement(
+        elem_inertial,
+        "origin",
+        {
+            "xyz": f"{cmass_pose.p[0]} {cmass_pose.p[1]} {cmass_pose.p[2]}",
+            "rpy": f"{angles[0]} {angles[1]} {angles[2]}",
+        },
+    )
+    ET.SubElement(elem_inertial, "mass", {"value": str(link.mass)})
+    ET.SubElement(
+        elem_inertial,
+        "inertia",
+        {
+            "ixx": str(link.inertia[0]),
+            "iyy": str(link.inertia[1]),
+            "izz": str(link.inertia[2]),
+            "ixy": "0",
+            "ixz": "0",
+            "iyz": "0",
+        },
+    )
+
+    return elem_link
+
+
+def export_joint(joint: PhysxArticulationJoint) -> list[ET.Element]:
+    """
+    Export an articulation joint. No joint mimic support yet.
+    If the joint has no parent_link (joint from root link to 1st robot link), generates
+    <joint name="__root_joint__" type="fixed">
+        <parent link="__world__"></parent>
+        <child link="link_base"></child>
+    </joint>
+
+    Otherwise, generates an extra dummy link and a dummy joint because SAPIEN allows
+    joint and child link to have different frames. Sample output:
+    <joint name="joint1" type="revolute">
+        <origin rpy="3.1415927410125732 -0.0 0.0" xyz="0.0 0.0 0.0"></origin>
+        <axis xyz="1 0 0"></axis>
+        <parent link="link_base"></parent>
+        <child link="link_dummy_1"></child>
+        <limit effort="100.0" lower="-6.2831855" upper="6.2831855" velocity="0"></limit>
+        <dynamics damping="50000.0" friction="0.05000000074505806"></dynamics>
+    </joint>
+    <link name="link_dummy_1"></link>
+    <joint name="joint_dummy_1" type="fixed">
+        <origin rpy="3.1415927410125732 -0.0 0.0" xyz="-1.0 1.0 2.0"></origin>
+        <parent link="link_dummy_1"></parent>
+        <child link="link_1"></child>
+    </joint>
+    """
+    if joint.parent_link is None:  # joint from root link to first robot link
+        type = "fixed" if joint.type == "fixed" else "floating"
+        elem_joint = ET.Element("joint", {"name": "__root_joint__", "type": type})
+        ET.SubElement(elem_joint, "parent", {"link": "__world__"})
+        ET.SubElement(elem_joint, "child", {"link": joint.child_link.name})
+        return [elem_joint]
+
+    if joint.type == "fixed":
+        type = "fixed"
+    elif joint.type == "prismatic":
+        type = "prismatic"
+    elif joint.type.startswith("revolute"):
+        if joint.limit[0][0] < -1e5 and joint.limit[0][1] > 1e5:
+            type = "continuous"
+        else:
+            type = "revolute"
+    else:
+        raise Exception(f"invalid joint type {joint.type}")
+
+    j2p = joint.pose_in_parent
+    c2j = joint.pose_in_child.inv()
+
+    # dummy link at joint frame
+    elem_dummy_link = ET.Element(
+        "link", {"name": f"link_dummy_{joint.child_link.index}"}
+    )
+
+    # joint connecting parent and dummy
+    elem_joint = ET.Element("joint", {"name": joint.name, "type": type})
+    angles = j2p.rpy
+    ET.SubElement(
+        elem_joint,
+        "origin",
+        {
+            "xyz": f"{j2p.p[0]} {j2p.p[1]} {j2p.p[2]}",
+            "rpy": f"{angles[0]} {angles[1]} {angles[2]}",
+        },
+    )
+    ET.SubElement(elem_joint, "axis", {"xyz": "1 0 0"})
+    ET.SubElement(elem_joint, "parent", {"link": joint.parent_link.name})
+    ET.SubElement(elem_joint, "child", {"link": f"link_dummy_{joint.child_link.index}"})
+    if type == "prismatic" or type == "revolute" or type == "continuous":
+        limit_attrib = {
+            "effort": str(min(joint.force_limit, 1e5)),
+            "velocity": "0",
+            "lower": str(max(joint.limit[0][0], -1e5)),
+            "upper": str(min(joint.limit[0][1], 1e5)),
+        }
+        if type == "continuous":
+            limit_attrib.pop("lower")
+            limit_attrib.pop("upper")
+
+        ET.SubElement(elem_joint, "limit", limit_attrib)
+        ET.SubElement(
+            elem_joint,
+            "dynamics",
+            {"damping": str(joint.damping), "friction": str(joint.friction)},
+        )
+        # TODO: joint mimic
+
+    # fixed joint connecting dummy and child
+    elem_dummy_joint = ET.Element(
+        "joint", {"name": f"joint_dummy_{joint.child_link.index}", "type": "fixed"}
+    )
+    angles = c2j.rpy
+    ET.SubElement(
+        elem_dummy_joint,
+        "origin",
+        {
+            "xyz": f"{c2j.p[0]} {c2j.p[1]} {c2j.p[2]}",
+            "rpy": f"{angles[0]} {angles[1]} {angles[2]}",
+        },
+    )
+    # NOTE: fixed joint does not need axis
+    # ET.SubElement(elem_dummy_joint, "axis", {"xyz": "0 0 0"})
+    ET.SubElement(
+        elem_dummy_joint, "parent", {"link": f"link_dummy_{joint.child_link.index}"}
+    )
+    ET.SubElement(elem_dummy_joint, "child", {"link": joint.child_link.name})
+
+    return [elem_joint, elem_dummy_link, elem_dummy_joint]
+
+
+def export_kinematic_chain_xml(articulation: PhysxArticulation) -> ET.Element:
+    elem_robot = ET.Element("robot", {"name": articulation.name})
+    ET.SubElement(elem_robot, "link", {"name": "__world__"})  # root link
+
+    for l in articulation.links:
+        elem_robot.append(export_link(l))
+
+    for j in articulation.joints:
+        elem_robot.extend(export_joint(j))
+
+    return elem_robot
+
+
+def export_kinematic_chain_urdf(
+    articulation: PhysxArticulation, force_fix_root: bool = False
+) -> str:
+    """
+    Exports the articulation's kinematic chain URDF as a string
+    (without visual/collision elements for links)
+
+    :param force_fix_root: Change all floating joints to fixed joints.
+    """
+    xml = export_kinematic_chain_xml(articulation)
+    if force_fix_root:
+        for j in xml.findall("joint"):
+            if j.attrib["type"] == "floating":
+                j.attrib["type"] = "fixed"
+
+    # Indent the XML for better readability (available only in python >= 3.9)
+    # ET.indent(xml, '  ')
+    return ET.tostring(xml, encoding="utf8").decode()
